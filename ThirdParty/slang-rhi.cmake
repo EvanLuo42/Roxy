3ce71de2ff6baee1caac9ba59dc49f4f7ba5b176
Evan Luo <evanluo@cs.ucsb.edu>
2026-07-25 00:04:53 +0800

    Bump to actions/checkout@v7

--- ThirdParty/slang-rhi.cmake
+++ ThirdParty/slang-rhi.cmake
@@ -1,4 +1,16 @@
+set(SLANG_RHI_BUILD_TESTS OFF)
+set(SLANG_RHI_BUILD_EXAMPLES OFF)
+set(SLANG_RHI_INSTALL OFF)
+
+set(SLANG_RHI_ENABLE_WGPU OFF)
+
 roxy_add_dependency(
         slang-rhi
         GIT_REPOSITORY https://github.com/shader-slang/slang-rhi.git
+        GIT_TAG 29dc332e55d8b6afa2e532cbf07d98d34bd9b029
+)
+
+add_executable(slangc IMPORTED GLOBAL)
+set_target_properties(slangc PROPERTIES
+        IMPORTED_LOCATION "${SLANG_RHI_SLANG_BINARY_DIR}/bin/slangc${CMAKE_EXECUTABLE_SUFFIX}"
 )
