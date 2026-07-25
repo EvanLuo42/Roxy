93f7c8d98384ac939a3f1de9f5de005655474999
Evan Luo <evanluo@cs.ucsb.edu>
2026-07-25 08:36:31 +0800

    Fix deps fetch issue

--- ThirdParty/slang-rhi.cmake
+++ ThirdParty/slang-rhi.cmake
@@ -7,7 +7,8 @@ set(SLANG_RHI_ENABLE_WGPU OFF)
 roxy_add_dependency(
         slang-rhi
         GIT_REPOSITORY https://github.com/shader-slang/slang-rhi.git
-        GIT_TAG 29dc332e55d8b6afa2e532cbf07d98d34bd9b029
+        GIT_TAG 1afb8384634bc8e9fc7ff2d75f8a0dbec5502f38
+        NO_SHALLOW
 )
 
 add_executable(slangc IMPORTED GLOBAL)
--- cmake/RoxyDependencies.cmake
+++ cmake/RoxyDependencies.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 function(roxy_add_dependency NAME)
     cmake_parse_arguments(
         ARG
-        ""
+        "NO_SHALLOW"
         "GIT_REPOSITORY;GIT_TAG"
         ""
         ${ARGN}
@@ -19,6 +19,15 @@ function(roxy_add_dependency NAME)
         set(ARG_GIT_TAG main)
     endif()
 
+    set(DECLARE_ARGS
+        GIT_REPOSITORY ${ARG_GIT_REPOSITORY}
+        GIT_TAG        ${ARG_GIT_TAG}
+        EXCLUDE_FROM_ALL
+    )
+    if(NOT ARG_NO_SHALLOW)
+        list(APPEND DECLARE_ARGS GIT_SHALLOW TRUE)
+    endif()
+
     message(STATUS "Fetching dependency ${NAME}")
     message(STATUS "  Repository: ${ARG_GIT_REPOSITORY}")
     message(STATUS "  Tag: ${ARG_GIT_TAG}")
@@ -26,13 +35,7 @@ function(roxy_add_dependency NAME)
     FetchContent_GetProperties(${NAME})
 
     if(NOT ${NAME}_POPULATED)
-        FetchContent_Declare(
-            ${NAME}
-            GIT_REPOSITORY ${ARG_GIT_REPOSITORY}
-            GIT_TAG        ${ARG_GIT_TAG}
-            GIT_SHALLOW    TRUE
-            EXCLUDE_FROM_ALL TRUE
-        )
+        FetchContent_Declare(${NAME} ${DECLARE_ARGS})
 
         FetchContent_MakeAvailable(${NAME})
     else()
